d60554cabf395ef4da31686d593ae136d1d53ff1
FrankSluyter <sluyterfrank@gmail.com>
2019-03-27 10:03:30 +1000

    Created first tool project
    Just starting the project

---
+++ YourProjectToolRental/YourProjectToolRental/Models/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YourProjectToolRental.Models
+{
+	public class Inventory
+	{
+		public int AssetId { get; set; }
+		public string Brand { get; set; }
+		public string Description { get; set; }
+		public bool Active { get; set; }
+		public string comment { get; set; }
+	}
+}
