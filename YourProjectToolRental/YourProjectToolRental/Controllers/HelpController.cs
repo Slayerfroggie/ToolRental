9072c63af9c4a0c51f3cee6e6d4633f730a39fc4
FrankSluyter <sluyterfrank@gmail.com>
2019-06-19 12:00:00 +1000

    added help page

---
+++ YourProjectToolRental/YourProjectToolRental/Controllers/HelpController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YourProjectToolRental.Controllers
+{
+    public class HelpController : Controller
+    {
+        // GET: Help
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult Inventory()
+		{
+			return View();
+		}
+
+		public ActionResult Rentals()
+		{
+			return View();
+		}
+
+		public ActionResult Customers()
+		{
+			return View();
+		}
+	}
+}
